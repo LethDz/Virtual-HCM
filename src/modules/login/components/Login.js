f373d6bd8e641466b6cf8b7802d1902a9861af1f
TungDS <kirisaki124@yahoo.com>
2020-10-07 00:36:08 +0700

    001/LoginPage/Fix coding convention

--- src/modules/login/components/Login.js
+++ src/modules/login/components/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import SignInForm from "./SignInForm";
+import SignInForm from "src/modules/login/components/SignInForm";
 import "src/static/stylesheets/login.css";
 
 class Login extends Component {
--- src/modules/login/components/SignInForm.js
+++ src/modules/login/components/SignInForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Form, Input, Button } from "reactstrap";
+import lockIcon from 'src/static/icons/lockIcon'
 
 class SignInForm extends Component {
   render() {
@@ -8,14 +9,7 @@ class SignInForm extends Component {
         <Form className="login-form-m align-center">
           <h5 className="login-form-title">Hồ Chí Minh Virtual Chatbot</h5>
           <div className="icon-lock align-center">
-            <svg
-              class="MuiSvgIcon-root lock-image"
-              focusable="false"
-              viewBox="0 0 24 24"
-              aria-hidden="true"
-            >
-              <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2z"></path>
-            </svg>
+            {lockIcon}
           </div>
 
           <Input
